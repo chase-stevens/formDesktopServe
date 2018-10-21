3adacdfaa79729693d4e614be41b6002b22ee20e
chase-stevens <chasestevenspersonal@gmail.com>
2018-10-21 13:25:52 -0700

    working version

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const { app, BrowserWindow, ipcMain, } = require('electron')
 const url = require('url');
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
 
 let mainWindow;
 let addWindow;
 let jsonQuestions = [];
-let formData = []
+let formData = [];
 
 app.on('ready', function(){
   // Create the browser window.
@@ -42,33 +42,72 @@ ipcMain.on('loadedFormQuestion', (event, arg) => {
   if (jsonQuestions.length > 0) {
     jsonQuestions = [];
   }
+  mainWindow.webContents.send('formData:clear');
   let textFormQuestions = JSON.parse(arg);
   let formData = [];
   let row = [];
-  mainWindow.webContents.send('formData:clear');
   for (let i = 0; i < textFormQuestions.length; i++) {
     jsonQuestions.push(textFormQuestions[i]);
     row.push(textFormQuestions[i].tag);
   }
-  formData.push(row);
   mainWindow.webContents.send('formData:add', row);
-})
+  //formData.push(row);
+  console.log(formData);
+});
 
 // clear form
 ipcMain.on('clearForm', function(){
-  formData = [];
   mainWindow.webContents.send('formData:clear');
+  formData = [];
   jsonQuestions = [];
 });
 
+// export data as csv
+ipcMain.on('exportToCSV', function(){
+  let date = new Date;
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let content = nestedArrayToCSV(formData);
+  let fName = year + '-' + month + "-" + day + ".csv";
+  let fDir = __dirname + "/csvData/" + fName;
+  fs.writeFile(fDir, content, (err) => {
+    if (err) { throw err };
+    console.log('The file has been saved!');
+  });
+});
+
+function nestedArrayToCSV(nestedArray) {
+  var lineArray = [];
+
+  console.log(jsonQuestions);
+
+  let header = []
+
+  for (let i = 0; i < jsonQuestions.length; i++) {
+    header.push(jsonQuestions[i].tag);
+  }
+
+  nestedArray.unshift(header);
+
+  for (let i = 0; i < nestedArray.length; i++) {
+    var line = nestedArray[i].join(",");
+    lineArray.push(line);
+  }
+
+  var csvContent = lineArray.join("\n");
+  console.log(`csv:\n${csvContent}`);
+  return csvContent;
+}
+
 // Catch new window event
 ipcMain.on('newForm:add', function(){
+  console.log(formData);
   createAddWindow();
 });
 
 // renders form to new window
 ipcMain.on('formQuestionsRequest', (event, arg) => {
-  console.log(arg) // prints "ping"
   addWindow.send("formQuestionsSend", {
     success: true,
     message: 'Loaded json',
@@ -78,7 +117,7 @@ ipcMain.on('formQuestionsRequest', (event, arg) => {
 
 // adds to form data
 ipcMain.on('formData:add', function(e, row){
-  formData.push(row)
+  formData.push(row);
   console.log(formData);
   mainWindow.webContents.send('formData:add', row);
   addWindow.close();
