683bc0f015ac3172b715a9bcbe17e940b33de941
chase-stevens <chasestevenspersonal@gmail.com>
2018-10-21 01:17:13 -0700

    added serve screen + keyboard shortcuts

--- main.js
+++ main.js
@@ -1,11 +1,97 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
+const url = require('url');
+const path = require('path');
+const fs = require('fs')
 
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
+let mainWindow;
+let addWindow;
 
-    // and load the index.html of the app.
-    win.loadFile('index.html')
+app.on('ready', function(){
+  // Create the browser window.
+  mainWindow = new BrowserWindow({});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+// Handle create add window
+function createAddWindow(){
+  // Create the browser window.
+  addWindow = new BrowserWindow({
+    width: 800,
+    height: 400,
+    title: 'Add Form'
+  });
+
+  // and load the index.html of the app.
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+}
+
+// Catch color:add
+ipcMain.on('color:add', function(e, color){
+  mainWindow.webContents.send('color:add', color);
+  addWindow.close();
+});
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+      label: "File",
+      submenu: [
+        {
+          label: 'Load Form',
+          accelerator: process.platform == 'darwin' ? 'Command+L' : 'Ctrl+L',
+          click(){
+            createAddWindow();
+          }
+        },
+        {
+          label: 'Clear Form'
+        },
+        {
+          label: 'Quit',
+          accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+          click(){
+            app.quit();
+          }
+        }
+      ]
   }
+];
 
-  app.on('ready', createWindow)
+// if mac, add empty object to menu
+ if (process.platform == 'darwin') {
+   mainMenuTemplate.unshift({});
+ }
+
+ // add developer tools item if not in prod
+ if(process.env.NODE_ENV !== 'production'){
+   mainMenuTemplate.push({
+     label: 'Developer Tools',
+     submenu:[
+       {
+         label: 'Toggle DevTools',
+         accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+         click(item, focusedWindow){
+           focusedWindow.toggleDevTools();
+         }
+       },
+       {
+          role: 'reload'
+       }
+     ]
+   })
+ }
